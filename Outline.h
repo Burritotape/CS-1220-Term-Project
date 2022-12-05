cc3201d95df5ecec836d2210e416131e7638612c
Jaedan Biermeier <jaedanjb@gmail.com>
2022-12-05 14:14:10 -0500

    outline update

--- Outline.h
+++ Outline.h
@@ -1,2 +1,15 @@
 #pragma once
 //Start of the outline for this project
+
+//Goal of this project is to be able to
+
+// 1) Accept a text and vector file 
+// 2) read and interpret both files
+// 3) simulate to given curcuit
+// 4) simulate the outputs of the file given the inputs in the vector file
+// 5) ouput the given events and the wave data
+
+
+// u
+
+
