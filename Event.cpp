72725dc6f52ae53d828fddb1dd511e39b571ca9a
Burritotape <113456142+Burritotape@users.noreply.github.com>
2022-12-09 06:58:09 -0500

    Event class comments complete

--- Event.cpp
+++ Event.cpp
@@ -8,45 +8,42 @@
 #include <string>
 #include "Event.h"
 
+// default event constructor
 Event::Event() {
 	wireNum = -1;
 	time = -1;
 	voltVal = 2;
 	OOArrival = -1;
 }
-
+// event constructor
 Event::Event(int I, int T, int V, int O) {
 	wireNum = I;
 	time = T;
 	voltVal = V;
 	OOArrival = O;
 }
-
+// returns event time
 int Event::GetTime() const {
 	return time;
 }
-
+// returns the number assigned to the wire
 int Event::GetWireNum() const{
 	return wireNum;
 }
-
+// returns value of the wire: hi, lo, or don't care
 int Event::GetVoltVal() const {
 	return voltVal;
 }
-
+// returns the order of arrival of the wire
 int Event::GetOOArrival() const {
 	return OOArrival;
 }
-
+// logic for priority_queue
 bool operator<(const Event& c1, const Event& c2) {
-
 	// by default the queue is a MAX queue, so we reverse the comparison
 	// from '<' to '>' (see the return statements) to make it a MIN queue
 	if (c1.time == c2.time) {
 		return c1.OOArrival > c2.OOArrival;
 	}
-	
 	return c1.OOArrival > c2.OOArrival;
-	
-	
 }
--- Event.h
+++ Event.h
@@ -5,18 +5,13 @@
 #include <iostream>
 using namespace std;
 
-class Event {   // take the information from the initially populated vectors to create events in a queue, which will be called in order during simulate
-public:
-    // public stuff goes here
+class Event {
     Event();
     Event(int I, int T, int V, int O);
-    int wireNum; // 
-    int time; // determines time of arrival; this is first priority for the queue
-    int voltVal; // determines whether the wire value goes high or low
-    int OOArrival; // Order_of_Arrival; this is second priority for sorting the queue
-    // constructor for events
-
-    // comparator for event order of operations
+    int wireNum;
+    int time;
+    int voltVal;
+    int OOArrival;
     friend bool operator<(const Event& c1, const Event& c2);
     int GetWireNum() const;
     int GetTime() const;
@@ -25,5 +20,5 @@ public:
 
 private:
     int k = 0;
-    vector<Event> priorityQueue; // for storing the queue of events to be read in simulation
+    vector<Event> priorityQueue;
 };
